020350c06aebb98ebc60f298dc6a93ce3c695098
dongdongdongk <dhk9309@gmail.com>
2023-01-14 19:31:03 +0900

    TryCatch

--- src/api/sec01/exam02/Key.java
+++ src/api/sec01/exam02/Key.java
@@ -1,5 +1,10 @@
 package api.sec01.exam02;
 
 public class Key {
-
+	public int number;
+	
+	public Key(int number) {
+		this.number = number;
+	}
+	
 }
--- src/tryCatch/sec01/exam01/TryCatchFinallyExample.java
+++ src/tryCatch/sec01/exam01/TryCatchFinallyExample.java
@@ -2,4 +2,11 @@ package tryCatch.sec01.exam01;
 
 public class TryCatchFinallyExample {
 
+	public static void main(String[] args) {
+		try {
+			Class clazz = Class.forName("java.lang.String2"); //클래스가 존재하지 않아 오류가 생긴다.
+		} catch(ClassNotFoundException e) {
+			System.out.println("클래스가 존재하지 않습니다.");
+		}
+	}
 }
